1535a2a135f4efc4b04efc836745ce647b48c820
SemWenders <s.wenders@student.fontys.nl>
2024-04-16 16:47:26 +0200

    added unit tests

---
+++ UserLogicTest/UserLogicTests.cs
@@ -0,0 +1,48 @@
+using DAL.Interfaces;
+using Logic;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Engine.ClientProtocol;
+using Moq;
+using System.Diagnostics;
+
+namespace UserLogicTest
+{
+    public class UserLogicTests
+    {
+        UserLogic userLogic;
+        List<string> friends;
+
+        public UserLogicTests()
+        {
+            var userRepoMock = new Mock<IUserRepository>();
+            userRepoMock.Setup(m => m.AcceptFriendRequest(It.IsAny<string>(), It.IsAny<string>())).Callback<string, string>((s, p) => friends.Add(s)).Returns(true);
+            userLogic = new UserLogic(userRepoMock.Object);
+            friends = new List<string>();
+            Debug.WriteLine("constructor executed");
+            
+        }
+
+        [Fact]
+        public void Test_RecipientAcceptsFriendRequest()
+        {
+            //arrange
+
+            //act
+            var result = userLogic.RespondToFriendRequest("responder", "sender", true);
+
+            //arrange
+            Assert.Contains("responder", friends);
+        }
+
+        [Fact]
+        public void Test_RecipientDeniesFriendRequest()
+        {
+            //arrange
+
+            //act
+            var result = userLogic.RespondToFriendRequest("responder", "sender", false);
+
+            //assert
+            Assert.DoesNotContain("responder", friends);
+        }
+    }
+}
