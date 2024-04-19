c9b9d88838b85052fe0a091b47e6fa03a8e2b475
SemWenders <s.wenders@student.fontys.nl>
2024-04-19 19:19:41 +0200

    added load tests

---
+++ LoadTest/scripts/GetFriends.js
@@ -0,0 +1,47 @@
+import http, { get } from "k6/http";
+import { check, sleep } from "k6";
+
+const isNumeric = (value) => /^\d+$/.test(value);
+
+const default_vus = 10;
+
+const target_vus_env = `${__ENV.TARGET_VUS}`;
+const host_url = __ENV.HOST_URL || "http://host.docker.internal:5166";
+const target_vus = isNumeric(target_vus_env)
+  ? Number(target_vus_env)
+  : default_vus;
+
+export let options = {
+  insecureSkipTLSVerify: true,
+  scenarios: {
+    getFriends: {
+      executor: "constant-vus",
+      duration: "20s",
+      exec: "getFriends",
+      vus: 10,
+    },
+    sendFriendRequest: {
+      executor: "constant-vus",
+      duration: "20s",
+      exec: "sendFriendRequest",
+      vus: 10,
+      startTime: "20s",
+    },
+  },
+};
+
+export function getFriends() {
+  const response = http.get(`${host_url}/Social/GetFriends/1`, {
+    headers: { Accepts: "application/json" },
+  });
+  check(response, { "status is 200": (r) => r.status === 200 });
+  sleep(0.3);
+}
+
+export function sendFriendRequest() {
+  const response = http.get(`${host_url}/Social/SendFriendRequest/2/3`, {
+    headers: { Accepts: "application/json" },
+  });
+  check(response, { "status is 200": (r) => r.status === 200 });
+  sleep(0.3);
+}
